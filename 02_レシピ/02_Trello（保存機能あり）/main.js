d9e365a85d16d1a9cecdeb1ad0471a3b4fd63573
nova-1001 <nova4710@outlook.jp>
2022-10-14 08:40:53 +0900

    [Trello (保存機能なし)]を学習しました。

---
+++ 02_レシピ/02_Trello（保存機能あり）/main.js
@@ -0,0 +1,18 @@
+const inputElement = document.getElementById("input-todo")
+const container = document.getElementById("cards-container")
+const addButton = document.getElementById("add-button")
+
+addButton.onclick = function () {
+  const text = inputElement.value
+
+  const card = document.createElement("div")
+  card.className = "card"
+
+  const todo = document.createElement("div")
+  todo.className = "todo"
+  todo.textContent = text
+
+  card.append(todo)
+
+  inputElement.value = ""
+}
